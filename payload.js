56699e54d6a5169ec11c2f5c341896ef2414efed
Noah Tajwar <noah.tajwar.oss@gmail.com>
2019-07-23 22:41:45 -0700

    Fixed bug

--- payload.js
+++ payload.js
@@ -25,7 +25,7 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
                         if ($(this).width() > 50 && $(this).height() > 50)
                         {
                             console.log("Sending Cognitive Services API request");
-                            describedImages.push(imgRet[0]);
+                            describedImages.push($(this)[0]);
 
                             CognitiveServicesAPI.getImageDescription($(this),
                                 function(firstCaption, imgRet)
